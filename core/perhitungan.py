1958ab8c9f084fe4ccbdad4d4da1b56a545ddfcd
cutwynona <wynonacut@gmail.com>
2025-10-07 16:25:14 +0700

    Update perhitungan.py

--- core/perhitungan.py
+++ core/perhitungan.py
@@ -1,1 +1,14 @@
+import numpy as np
+import pandas as pd
+from tabulate import tabulate
 
+hasil = {"tabel": []}
+
+def eval_fungsi(expr, x):
+    return eval(expr, {"np": np, "x": x})
+
+def hitung_metode_tabel(expr, N, bawah, atas):
+    h = (atas - bawah) / N
+    hasil["tabel"].clear()
+
+    iterasi_terpilih, x_terpilih = None, None
