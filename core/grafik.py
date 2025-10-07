5a59677e48ed55795d5358dd045a1ea4e82841a8
AndreaWidi <andreawidi886@gmail.com>
2025-10-07 16:43:40 +0700

    Update grafik.py

--- core/grafik.py
+++ core/grafik.py
@@ -2,4 +2,8 @@ import matplotlib.pyplot as plt
 
 def tampilkan_grafik(hasil):
     x_vals = [row[0] for row in hasil]
-    
+      y_vals = [row[1] for row in hasil]
+
+    plt.figure(figsize=(9, 5))
+    plt.plot(x_vals, y_vals, "o-b", label="f(x)")
+
