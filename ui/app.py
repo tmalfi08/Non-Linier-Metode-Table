141ac12d90b0530453ce070dd7c7b0edcdd794ad
coklatcake <najiyairawan06@gmail.com>
2025-10-07 18:23:11 +0700

    Update app.py

--- ui/app.py
+++ ui/app.py
@@ -4,3 +4,10 @@ from core.perhitungan import hitung_metode_tabel, hasil
 from core.grafik import tampilkan_grafik
 from tabulate import tabulate
 
+def start_app():
+    root = tk.Tk()
+    root.title("Metode Tabel - Kelompok 1")
+    root.geometry("650x750")
+    root.configure(bg="#ECECEC")
+
+
