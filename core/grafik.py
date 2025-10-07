86f3d9bd043b78ee591d5321b81facc2b0d99b01
AndreaWidi <andreawidi886@gmail.com>
2025-10-07 16:44:24 +0700

    Update grafik.py

--- core/grafik.py
+++ core/grafik.py
@@ -6,4 +6,6 @@ def tampilkan_grafik(hasil):
 
     plt.figure(figsize=(9, 5))
     plt.plot(x_vals, y_vals, "o-b", label="f(x)")
+ plt.axhline(0, color="red", linestyle="--")
+    plt.axvline(0, color="black", linestyle="--", linewidth=0.7)
 
