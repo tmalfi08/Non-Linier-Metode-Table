4cc506d1fcd7d42093625bb58b18fa9ca2275496
cutwynona <wynonacut@gmail.com>
2025-10-07 16:34:57 +0700

    Update perhitungan.py

--- core/perhitungan.py
+++ core/perhitungan.py
@@ -12,3 +12,13 @@ def hitung_metode_tabel(expr, N, bawah, atas):
     hasil["tabel"].clear()
 
     iterasi_terpilih, x_terpilih = None, None
+    
+    for i in range(N + 1):
+
+    for i in range(N + 1):
+        xi = bawah + i * h
+        fxi = eval_fungsi(expr, xi)
+        fxi_next = eval_fungsi(expr, xi + h) if i < N else None
+        perkalian = fxi * fxi_next if fxi_next is not None else None
+
+        hasil["tabel"].append([xi, fxi, fxi_next, perkalian])
