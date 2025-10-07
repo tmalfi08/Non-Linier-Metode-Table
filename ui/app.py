ce92aeedd4e6fe25fcaf35f19e97c83d04dfe7ac
coklatcake <najiyairawan06@gmail.com>
2025-10-07 18:22:40 +0700

    Update app.py

--- ui/app.py
+++ ui/app.py
@@ -1,1 +1,6 @@
+import tkinter as tk
+from tkinter import messagebox
+from core.perhitungan import hitung_metode_tabel, hasil
+from core.grafik import tampilkan_grafik
+from tabulate import tabulate
 
